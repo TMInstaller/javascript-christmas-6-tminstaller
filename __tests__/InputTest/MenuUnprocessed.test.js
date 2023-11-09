d1bc9c44305310715e0bed7c354948959484cdb1
TMInstaller <ajvldmlqjqclr@naver.com>
2023-11-10 02:17:29 +0900

    test(케이스): 테스트 케이스 data 변경

--- __tests__/InputTest/MenuUnprocessed.test.js
+++ __tests__/InputTest/MenuUnprocessed.test.js
@@ -10,7 +10,7 @@ describe("MenuUnprocessed 모델 테스트", () => {
   });
 
   test("입력받은 메뉴를 각 메뉴로 나누었을 때 올바른 형식으로 각각의 값이 이루어져있는 지 확인", async () => {
-    const input = "[[메뉴-구매수량],[메뉴-구매수량]],[메뉴&구매수량]]";
+    const input = "메뉴-구매수량,메뉴-구매수량,메뉴&구매수량";
     expect(() => {
       new MenuUnprocessed(input);
     }).toThrow(ERROR_CONVENTION);
