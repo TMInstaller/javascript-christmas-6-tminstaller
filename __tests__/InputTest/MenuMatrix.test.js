f1166e0b8c584562c83e6271c23fecac0f88b217
TMInstaller <ajvldmlqjqclr@naver.com>
2023-11-11 00:07:40 +0900

    test(메뉴): 미사용 테스트 주석처리

--- __tests__/InputTest/MenuMatrix.test.js
+++ __tests__/InputTest/MenuMatrix.test.js
@@ -1,5 +1,5 @@
 import { ERROR_CONVENTION } from "../../src/constants/convention.js";
-import { EVENT, MENU_COUNT } from "../../src/constants/number.js";
+import { MENU_COUNT } from "../../src/constants/number.js";
 import { MenuMatrix } from "../../src/models/MenuMatrix.js";
 
 describe("MenuMatrix 모델 테스트", () => {
@@ -14,15 +14,18 @@ describe("MenuMatrix 모델 테스트", () => {
     }).toThrow(ERROR_CONVENTION);
   });
 
-  test(`배열 안에 들어있는 모든 메뉴 가격 합이 ${EVENT.minimumAmount}를 넘지 않는다면 예외를 발생시킨다.`, async () => {
-    const input = [["시저샐러드", 1]];
-    expect(() => {
-      new MenuMatrix(input);
-    }).toThrow(ERROR_CONVENTION);
-  });
+  // test(`배열 안에 들어있는 모든 메뉴 가격 합이 ${EVENT.minimumAmount}를 넘지 않는다면 부적격 메세지를 반환한다.`, async () => {
+  //   const input = [["시저샐러드", 1]];
+  //   expect(() => {
+  //     new MenuMatrix(input);
+  //   }).toBe("이벤트 적용 불가");
+  // });
 
   test(`배열 내 서로다른 주문내역에 같은 이름의 상품이 존재하면 예외를 발생시킨다.`, async () => {
-    const input = [["시저샐러드", 1]];
+    const input = [
+      ["시저샐러드", 1],
+      ["시저샐러드", 3],
+    ];
     expect(() => {
       new MenuMatrix(input);
     }).toThrow(ERROR_CONVENTION);
