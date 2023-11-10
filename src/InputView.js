8507179a729a3568579ee1f7a6bdd24f5dd379e8
TMInstaller <ajvldmlqjqclr@naver.com>
2023-11-10 13:38:16 +0900

    feat(View): InputView 구현

--- src/InputView.js
+++ src/InputView.js
@@ -1,7 +1,52 @@
-export default InputView = {
-    async readDate() {
-        const input = await Console.readLineAsync("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
-        // ...
+import { Console } from "@woowacourse/mission-utils";
+import { EMPTY_LINE, INPUT_MESSAGE } from "./constants/message.js";
+import OutputView from "./OutputView.js";
+import { divideData, stringToNumber } from "./utils/conversion.js";
+import { VisitDate } from "./models/VisitDate.js";
+import { MenuUnprocessed } from "./models/MenuUnprocessed.js";
+import { DIVIDE_CONVENTION } from "./constants/convention.js";
+import { MenuMatrix } from "./models/MenuMatrix.js";
+import { MenuCount } from "./models/MenuCount.js";
+import { MenuName } from "./models/MenuName.js";
+
+const InputView = {
+  async readVisitDate() {
+    while (true) {
+      try {
+        Console.print(INPUT_MESSAGE.askWhenToVisit);
+        const visitDate = await Console.readLineAsync(EMPTY_LINE);
+        new VisitDate(visitDate);
+
+        // 이후 숫자로 변환
+        const visitDateToNumber = stringToNumber(visitDate);
+        return visitDateToNumber;
+      } catch (error) {
+        OutputView.printError(error);
+      }
     }
-    // ...
-}
+  },
+  async readMenu() {
+    while (true) {
+      try {
+        Console.print(INPUT_MESSAGE.askMenuToOrder);
+        const menuUnprocessed = await Console.readLineAsync(EMPTY_LINE);
+        new MenuUnprocessed(menuUnprocessed);
+        const menuArray = divideData(menuUnprocessed, DIVIDE_CONVENTION.menu);
+        const menuMatrix = menuArray.map((item) =>
+          divideData(item, DIVIDE_CONVENTION.nameAndCount)
+        );
+        for (const menu of menuMatrix) {
+          new MenuName(menu[0]);
+          new MenuCount(menu[1]);
+          menu[1] = stringToNumber(menu[1]);
+        }
+        new MenuMatrix(menuMatrix);
+        return menuMatrix;
+      } catch (error) {
+        OutputView.printError(error);
+      }
+    }
+  },
+};
+
+export default InputView;
