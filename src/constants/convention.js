3fce26173516724dc1cb6fb1ece78cfdc8d0095f
TMInstaller <ajvldmlqjqclr@naver.com>
2023-11-10 14:30:24 +0900

    feat(상수): 상수 추가

--- src/constants/convention.js
+++ src/constants/convention.js
@@ -1,5 +1,11 @@
 export const ERROR_CONVENTION = "[ERROR]";
+
 export const DIVIDE_CONVENTION = Object.freeze({
   menu: ",",
   nameAndCount: "-",
 });
+
+export const NOTHING_CONVENTION = "없음";
+
+// TODO: 이 부분은 CONVENTION이라는 하나의 객체로 묶을 지 고민
+// 묶는다면 menu -> menuDivision 이런 식으로 될 듯
