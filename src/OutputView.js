03c13b5a5cafcfa0f5895fd5027f8b789579ab7f
TMInstaller <ajvldmlqjqclr@naver.com>
2023-11-10 14:29:51 +0900

    feat(OutputView): 조건을 지정하지 않은 출력 기능 추가

--- src/OutputView.js
+++ src/OutputView.js
@@ -1,12 +1,69 @@
 import { Console } from "@woowacourse/mission-utils";
+import { BENEFITS_MESSAGE, EMPTY_LINE } from "./constants/message.js";
+import { NOTHING_CONVENTION } from "./constants/convention.js";
 
 const OutputView = {
-  printMenu() {
-    Console.print("<주문 메뉴>");
-    // ...
+  printDivideSection() {
+    Console.print(EMPTY_LINE);
   },
+
   printError(error) {
     Console.print(error);
+  },
+
+  printNothing() {
+    Console.print(NOTHING_CONVENTION);
+  },
+
+  printBenefitsIntro() {
+    Console.print(BENEFITS_MESSAGE.introducePreview);
+    this.printDivideSection();
+  },
+
+  printOrderedMenu(orderedMenu) {
+    Console.print(BENEFITS_MESSAGE.orderedMenu);
+    Console.print(
+      orderedMenu
+        .map(([menuItem, quantity]) => `${menuItem} ${quantity}개`)
+        .join("\n")
+    );
+    this.printDivideSection();
+  },
+
+  printPriceBeforeDiscount() {
+    Console.print(BENEFITS_MESSAGE.priceBeforeDiscount);
+    Console.print(NOTHING_CONVENTION);
+    Console.print(EMPTY_LINE);
+  },
+
+  printSatisfiedGiveAwayMenu() {
+    Console.print(BENEFITS_MESSAGE.giveawayMenu);
+    Console.print(NOTHING_CONVENTION);
+    Console.print(EMPTY_LINE);
+  },
+
+  printBenefits() {
+    Console.print(BENEFITS_MESSAGE.benefits);
+    Console.print(NOTHING_CONVENTION);
+    Console.print(EMPTY_LINE);
+  },
+
+  printTotalBenefitsPrice() {
+    Console.print(BENEFITS_MESSAGE.totalBenefitsPrice);
+    Console.print(NOTHING_CONVENTION);
+    Console.print(EMPTY_LINE);
+  },
+
+  printPriceAfterDiscount() {
+    Console.print(BENEFITS_MESSAGE.priceAfterDiscount);
+    Console.print(NOTHING_CONVENTION);
+    Console.print(EMPTY_LINE);
+  },
+
+  printSatisfiedEventBadge() {
+    Console.print(BENEFITS_MESSAGE.eventBadge);
+    Console.print(NOTHING_CONVENTION);
+    Console.print(EMPTY_LINE);
   },
 };
 
