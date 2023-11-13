d6289398d406973c1608a7a13e9ff5a867dc4b69
TMInstaller <ajvldmlqjqclr@naver.com>
2023-11-13 20:34:04 +0900

    feat(App): 모듈화

--- src/App.js
+++ src/App.js
@@ -11,26 +11,66 @@ class App {
 
   async run() {
     OutputView.printStartMessage();
+    const { dateToVisit, orderedMenu } = await this.readInputs();
+    const { amountBeforeDiscount, orderedCategories } =
+      this.calculateDiscounts(orderedMenu);
+
+    const resultsData = {
+      orderedMenu,
+      dateToVisit,
+      amountBeforeDiscount,
+      orderedCategories,
+    };
+
+    this.printResults(resultsData);
+  }
+
+  async readInputs() {
     const dateToVisit = await InputView.readVisitDate();
     const orderedMenu = await InputView.readMenu();
+    return { dateToVisit, orderedMenu };
+  }
+
+  calculateDiscounts(orderedMenu) {
     const amountBeforeDiscount =
       this.discountCalculator.calculateTotalAmount(orderedMenu);
     const orderedCategories =
       this.eventCheckManager.checkHowManyMenuInCategory(orderedMenu);
+    return { amountBeforeDiscount, orderedCategories };
+  }
+
+  printResults(resultsData) {
+    this.printOrderDetails(resultsData);
+    this.printBenefitsDetails(resultsData);
+    this.printFinalResults(resultsData);
+  }
+
+  printOrderDetails({ orderedMenu, amountBeforeDiscount }) {
     OutputView.printOrderedMenu(orderedMenu);
     OutputView.printPriceBeforeDiscount(amountBeforeDiscount);
     OutputView.printSatisfiedGiveAwayMenu(amountBeforeDiscount);
+  }
+
+  printBenefitsDetails({
+    dateToVisit,
+    amountBeforeDiscount,
+    orderedCategories,
+  }) {
     const totalBenefitsPrice = OutputView.printBenefits(
       dateToVisit,
       amountBeforeDiscount,
       orderedCategories
     );
-    OutputView.printTotalBenefitsPrice(totalBenefitsPrice);
+    this.totalBenefitsPrice = totalBenefitsPrice; // 저장하여 다음 단계에서 사용
+  }
+
+  printFinalResults({ amountBeforeDiscount }) {
+    OutputView.printTotalBenefitsPrice(this.totalBenefitsPrice);
     OutputView.printPriceAfterDiscount(
       amountBeforeDiscount,
-      totalBenefitsPrice
+      this.totalBenefitsPrice
     );
-    OutputView.printSatisfiedEventBadge(totalBenefitsPrice);
+    OutputView.printSatisfiedEventBadge(this.totalBenefitsPrice);
   }
 }
 
