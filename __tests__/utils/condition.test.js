912ef23cf1de2dbd20926addd9f6ddc3981e8566
TMInstaller <ajvldmlqjqclr@naver.com>
2023-11-14 14:13:30 +0900

    test(utils): condition 함수 테스트 추가

---
+++ __tests__/utils/condition.test.js
@@ -0,0 +1,198 @@
+import { ERROR_CONVENTION } from "../../src/constants/convention";
+import {
+  checkIsEmpty,
+  checkIsInteger,
+  checkIsNumber,
+  checkIsOverMaximum,
+  checkIsUnderMinimum,
+  checkIsIncluded,
+  checkIsPositiveNumber,
+  checkIsNotZero,
+  checkIsTypeObject,
+  checkIsUnder,
+  checkIsOver,
+} from "../../src/utils/condition";
+
+describe("checkIsEmpty 함수 테스트", () => {
+  test("빈 문자열이 입력되면 에러가 발생한다", () => {
+    expect(() => checkIsEmpty("")).toThrow(ERROR_CONVENTION);
+  });
+
+  test("공백만 있는 문자열이 입력되면 에러가 발생한다", () => {
+    expect(() => checkIsEmpty("   ")).toThrow(ERROR_CONVENTION);
+  });
+
+  test("정상적인 문자열이 입력되면 에러가 발생하지 않는다", () => {
+    expect(() => checkIsEmpty("Hello World")).not.toThrow();
+  });
+
+  test("null이 입력되면 에러가 발생한다", () => {
+    expect(() => checkIsEmpty(null)).toThrow(ERROR_CONVENTION);
+  });
+
+  test("undefined가 입력되면 에러가 발생한다", () => {
+    expect(() => checkIsEmpty(undefined)).toThrow(ERROR_CONVENTION);
+  });
+});
+
+describe("checkIsNumber 함수 테스트", () => {
+  const errorMessage = ERROR_CONVENTION;
+
+  test("숫자 형태의 문자열이 입력되면 에러가 발생하지 않는다", () => {
+    expect(() => checkIsNumber("123", errorMessage)).not.toThrow();
+  });
+
+  test("숫자가 입력되면 에러가 발생하지 않는다", () => {
+    expect(() => checkIsNumber(123, errorMessage)).not.toThrow();
+  });
+
+  test("숫자가 아닌 문자열이 입력되면 에러가 발생한다", () => {
+    expect(() => checkIsNumber("abc", errorMessage)).toThrow(errorMessage);
+  });
+
+  test("null이 입력되면 에러가 발생한다", () => {
+    expect(() => checkIsNumber(null, errorMessage)).toThrow(errorMessage);
+  });
+
+  test("undefined가 입력되면 에러가 발생한다", () => {
+    expect(() => checkIsNumber(undefined, errorMessage)).toThrow(errorMessage);
+  });
+});
+
+describe("checkIsUnderMinimum 함수 테스트", () => {
+  const minimumNumber = 10;
+
+  test("data가 minNumber보다 작으면 에러가 발생한다", () => {
+    expect(() => checkIsUnderMinimum(5, minimumNumber)).toThrow(
+      ERROR_CONVENTION
+    );
+  });
+
+  test("data가 minNumber와 같으면 에러가 발생하지 않는다", () => {
+    expect(() => checkIsUnderMinimum(10, minimumNumber)).not.toThrow();
+  });
+
+  test("data가 minNumber보다 크면 에러가 발생하지 않는다", () => {
+    expect(() => checkIsUnderMinimum(15, minimumNumber)).not.toThrow();
+  });
+});
+
+describe("checkIsOverMaximum 함수 테스트", () => {
+  const maxNumber = 10;
+
+  test("data가 maxNumber보다 크면 에러가 발생한다", () => {
+    expect(() => checkIsOverMaximum(15, maxNumber)).toThrow(ERROR_CONVENTION);
+  });
+
+  test("data가 maxNumber와 같으면 에러가 발생하지 않는다", () => {
+    expect(() => checkIsOverMaximum(10, maxNumber)).not.toThrow();
+  });
+
+  test("data가 maxNumber보다 작으면 에러가 발생하지 않는다", () => {
+    expect(() => checkIsOverMaximum(5, maxNumber)).not.toThrow();
+  });
+});
+
+describe("checkIsInteger 함수 테스트", () => {
+  test("정수가 입력되면 에러가 발생하지 않는다", () => {
+    expect(() => checkIsInteger(10)).not.toThrow();
+  });
+
+  test("소수가 입력되면 에러가 발생한다", () => {
+    expect(() => checkIsInteger(10.5)).toThrow(ERROR_CONVENTION);
+  });
+
+  test("숫자가 아닌 값이 입력되면 에러가 발생한다", () => {
+    expect(() => checkIsInteger("abc")).toThrow(ERROR_CONVENTION);
+    expect(() => checkIsInteger({})).toThrow(ERROR_CONVENTION);
+  });
+
+  test("null이 입력되면 에러가 발생한다", () => {
+    expect(() => checkIsInteger(null)).toThrow(ERROR_CONVENTION);
+  });
+
+  test("undefined가 입력되면 에러가 발생한다", () => {
+    expect(() => checkIsInteger(undefined)).toThrow(ERROR_CONVENTION);
+  });
+});
+
+describe("checkIsIncluded 함수 테스트", () => {
+  const array = [1, 2, 3, 4, 5];
+
+  test("배열에 요소가 포함되어 있으면 에러가 발생하지 않는다", () => {
+    expect(() => checkIsIncluded(array, 3)).not.toThrow();
+  });
+
+  test("배열에 요소가 포함되어 있지 않으면 에러가 발생한다", () => {
+    expect(() => checkIsIncluded(array, 6)).toThrow(ERROR_CONVENTION);
+  });
+
+  test("data가 배열이 아니면 에러가 발생한다", () => {
+    expect(() => checkIsIncluded("not an array", 1)).toThrow(ERROR_CONVENTION);
+    expect(() => checkIsIncluded(null, 1)).toThrow(ERROR_CONVENTION);
+    expect(() => checkIsIncluded(undefined, 1)).toThrow(ERROR_CONVENTION);
+  });
+});
+
+describe("checkIsPositiveNumber 함수 테스트", () => {
+  test("양수에 대해 true를 반환한다", () => {
+    expect(checkIsPositiveNumber(5)).toBe(true);
+  });
+
+  test("0에 대해 false를 반환한다", () => {
+    expect(checkIsPositiveNumber(0)).toBe(false);
+  });
+
+  test("음수에 대해 false를 반환한다", () => {
+    expect(checkIsPositiveNumber(-5)).toBe(false);
+  });
+
+  test("NaN에 대해 false를 반환한다", () => {
+    expect(checkIsPositiveNumber(NaN)).toBe(false);
+  });
+});
+
+describe("checkIsNotZero 함수 테스트", () => {
+  test("0에 대해 false를 반환한다", () => {
+    expect(checkIsNotZero(0)).toBe(false);
+  });
+
+  test("0이 아닌 숫자에 대해 true를 반환한다", () => {
+    expect(checkIsNotZero(5)).toBe(true);
+    expect(checkIsNotZero(-5)).toBe(true);
+  });
+});
+
+describe("checkIsTypeObject 함수 테스트", () => {
+  test("객체에 대해 true를 반환한다", () => {
+    expect(checkIsTypeObject({})).toBe(true);
+    expect(checkIsTypeObject([])).toBe(true);
+  });
+
+  test("비객체에 대해 false를 반환한다", () => {
+    expect(checkIsTypeObject(5)).toBe(false);
+    expect(checkIsTypeObject(null)).toBe(false); // null은 typeof에서 "object"를 반환하지만, 일반적인 객체 테스트 목적에는 부합하지 않을 수 있음
+  });
+});
+
+describe("checkIsUnder 함수 테스트", () => {
+  test("지정된 값보다 작은 경우 true를 반환한다", () => {
+    expect(checkIsUnder(5, 10)).toBe(true);
+  });
+
+  test("지정된 값보다 크거나 같은 경우 false를 반환한다", () => {
+    expect(checkIsUnder(10, 10)).toBe(false);
+    expect(checkIsUnder(15, 10)).toBe(false);
+  });
+});
+
+describe("checkIsOver 함수 테스트", () => {
+  test("지정된 값보다 크거나 같은 경우 true를 반환한다", () => {
+    expect(checkIsOver(10, 10)).toBe(true);
+    expect(checkIsOver(15, 10)).toBe(true);
+  });
+
+  test("지정된 값보다 작은 경우 false를 반환한다", () => {
+    expect(checkIsOver(5, 10)).toBe(false);
+  });
+});
--- src/utils/condition.js
+++ src/utils/condition.js
@@ -1,57 +1,48 @@
 import { ERROR_MESSAGE } from "../constants/error.js";
 
 export const checkIsEmpty = (data) => {
-  if (!data.trim().length) {
+  if (!data?.trim().length) {
     throw new Error(ERROR_MESSAGE.isEmpty);
   }
 };
-
-export const checkIsNumber = (data, errMessage) => {
-  if (isNaN(data)) {
-    throw new Error(errMessage);
-  }
-};
-
-export const checkIsUnderMinimum = (data, minNumber) => {
-  if (data < minNumber) {
-    throw new Error(ERROR_MESSAGE.isUnderMinimum);
-  }
-};
-
-export const checkIsOverMaximum = (data, maxNumber) => {
-  if (data > maxNumber) {
-    throw new Error(ERROR_MESSAGE.isOverMaximum);
-  }
-};
-
 export const checkIsInteger = (data) => {
   if (!Number.isInteger(data)) {
     throw new Error(ERROR_MESSAGE.isNotInteger);
   }
 };
-
+export const checkIsNumber = (data, errMessage) => {
+  if (isNaN(data) || data === null) {
+    throw new Error(errMessage);
+  }
+};
+export const checkIsOverMaximum = (data, maxNumber) => {
+  if (data > maxNumber) {
+    throw new Error(ERROR_MESSAGE.isOverMaximum);
+  }
+};
+export const checkIsUnderMinimum = (data, minNumber) => {
+  if (data < minNumber) {
+    throw new Error(ERROR_MESSAGE.isUnderMinimum);
+  }
+};
 export const checkIsIncluded = (data, element) => {
-  if (!data.includes(element)) {
+  if (!data?.includes(element)) {
     throw new Error(ERROR_MESSAGE.isInvalidInput);
   }
 };
 
 export const checkIsPositiveNumber = (data) => {
-  return data !== 0 && !isNaN(data);
+  return data !== 0 && !isNaN(data) && data > 0;
 };
-
 export const checkIsNotZero = (data) => {
   return data !== 0;
 };
-
 export const checkIsTypeObject = (data) => {
-  return typeof data === "object";
+  return typeof data === "object" && data !== null;
 };
-
 export const checkIsUnder = (data, value) => {
   return data < value;
 };
-
 export const checkIsOver = (data, value) => {
   return data >= value;
 };
